ca08ee5ef2b86e6f450a4aff71669e47cd5b7489
syuanuvt <s.yuan@uvt.nl>
2018-01-08 16:10:26 +0100

    this function is not useful

--- model selection.R
+++
@@ -1,7 +0,0 @@
-# The code which could select the total number of components and clusters 
-# created by Yuan Shuai 25/10/2017
-
-# check
-CSSCA_selection1<- function(maxcluster = 11, maxcomponent = 11) {
-  
-}
